af328850dec4fb61daa86c8a16b07f5aaf98d727
Mohamed Magdy <moh.magdy.abden2016@gmail.com>
2022-11-12 02:33:35 +0200

    adding not null constraint

--- src/main/java/com/musala/soft/drones/entity/Drone.java
+++ src/main/java/com/musala/soft/drones/entity/Drone.java
@@ -15,19 +15,21 @@ import javax.persistence.*;
 @Table(name = "drone")
 public class Drone extends BaseEntity {
 
-    @Column(name = "serial_number", unique = true, updatable = false, length = 100)
+    @Column(name = "serial_number", unique = true, updatable = false, length = 100, nullable = false)
     private String serialNumber;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "model", nullable = false)
     private Model model;
 
-    @Column(name = "weight_limit")
+    @Column(name = "weight_limit", nullable = false)
     private Double weightLimit;
 
-    @Column(name = "battery_capacity")
+    @Column(name = "battery_capacity", nullable = false)
     private Double batteryCapacity;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false)
     private State state;
 
 }
